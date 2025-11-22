618e1c79c38b9ce311a796ea30babfd356d7d45a
martin-santiago <mprez@uc.cl>
2025-11-22 02:07:48 -0300

    chore/railway-deploy

--- main.py
+++ main.py
@@ -70,4 +70,7 @@ def health():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    import os
+
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
